91794d593d1a638271c47fbc13d48d747b0034b7
Pei Deng <pei_mbp@Peis-MacBook-Pro.local>
2018-03-14 22:10:11 -0700

    removed omf intergrations

--- config.fish
+++ config.fish
@@ -1,10 +1,3 @@
-# Path to Oh My Fish install.
-set -q XDG_DATA_HOME
-  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
-  or set -gx OMF_PATH "$HOME/.local/share/omf"
-
-# Load Oh My Fish configuration.
-source $OMF_PATH/init.fish
 # alias
 alias s='subl'
 alias gcmsg='git commit -m'
@@ -48,10 +41,3 @@ function unset
 end
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
-
-# tabtab source for serverless package
-# uninstall by removing these lines or running `tabtab uninstall serverless`
-[ -f /Users/pei/.nvm/versions/node/v6.1.0/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish ]; and . /Users/pei/.nvm/versions/node/v6.1.0/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish
-# tabtab source for sls package
-# uninstall by removing these lines or running `tabtab uninstall sls`
-[ -f /Users/pei/.nvm/versions/node/v6.1.0/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish ]; and . /Users/pei/.nvm/versions/node/v6.1.0/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish
