6a104d3b78c0436a47b48c23e284af5e34ce6715
Pei Deng <p31.d3ng@gmail.com>
2018-07-18 22:24:14 -0700

    remove extra

--- functions/publ-ip-exists.fish
+++
@@ -1,9 +0,0 @@
-function publ-ip-exists
-    if count $argv > /dev/null
-        /Users/pei/Securly/devops/aws-cli/prod-publ-ip-exists.sh $argv
-    else
-        echo "Usage: publ-ip-exists [public_ip]"
-        echo "Example: publ-ip-exists 50.18.216.174"
-    end
-end
-
